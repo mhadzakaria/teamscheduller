32de830ea11b245af816ed6b9d44177fec9a81ef
Ahmad Zak <azak808@gmail.com>
2017-11-16 08:56:52 +0700

    add pagination for position controller

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -3,7 +3,7 @@ class PositionsController < ApplicationController
 	before_action :authenticate_user!
 
 	def index
-		@positions = Position.all
+		@positions = Position.paginate(:page => params[:page], :per_page => 5)
 	end
 
 	def new
